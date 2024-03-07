c282d0ba7724759d931b9aba38a5aca9d5969a9b
shubham <shubhamsharma7120@gmail.com>
2024-03-08 02:05:38 +0530

    Latest code after added clients

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  autoplay:true,
+  dots:false,
+  responsive:{
+      0:{
+          items:3
+      },
+      600:{
+          items:5
+      },
+      1000:{
+          items:5
+      }
+  }
+})
 
 function submitForm() {
   // Prevent default form submission if needed
